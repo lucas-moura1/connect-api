148b3c1531000294d4be0b364f4c4b77d3bd6d6b
Lucas Moura <lucas3ms.ls@gmail.com>
2022-03-25 15:33:00 -0300

    Add inital routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import * as cultController from '../controllers/cultController.js'
+import * as connectController from '../controllers/connectController.js'
 
 const route = express.Router()
 
-route.get('/', (req, res) => {
+route.get('/health', (req, res) => {
     res.json({ status: 'UP!' })
 })
+
+route.get('/cult', cultController.getAllCult)
+route.post('/cult', cultController.createCult)
+route.post('/connect', connectController.createConnect)
 
 export default route
