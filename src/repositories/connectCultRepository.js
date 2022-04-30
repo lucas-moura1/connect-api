fd33eed53e8332cd64bc47b75bbb8362b401b634
Lucas Moura <lucas3ms.ls@gmail.com>
2022-04-30 10:52:26 -0300

    feat: remove not used function and change function that make association between connect and cult

--- src/repositories/connectCultRepository.js
+++ src/repositories/connectCultRepository.js
@@ -2,29 +2,13 @@ import { ConnectCulto, Connect, Culto } from '../models/index.js'
 import logger from '../config/logger.js'
 import { RequestError } from '../errors/RequestError.js'
 
-const getAll = async () => {
-    try {
-        logger.info('[CONNECT REPOSITORY] Getting all connectors')
-
-        const connectors = await ConnectCulto.findAll()
-
-        return connectors
-    } catch (error) {
-        logger.error(
-            `[CONNECT REPOSITORY] Error to getting all connectors >> ${JSON.stringify(error)}`
-        )
-
-        throw new RequestError()
-    }
-}
-
-const create = async ({ connectDatas, cultoDatas, numeroPulseira, observacoes }) => {
+const associate = async ({ connectDatas, cultoDatas, numeroPulseira, observacoes }) => {
     try {
         logger.info('[CONNECT REPOSITORY] Creating a connect cult')
 
-        const pulseiraData = { through: { numeroPulseira, observacoes } }
+        const throughDatas = { through: { numeroPulseira, observacoes } }
 
-        const connectCult = await connectDatas.addCulto(cultoDatas, pulseiraData)
+        const connectCult = await connectDatas.addCulto(cultoDatas, throughDatas)
 
         return connectCult
     } catch (error) {
@@ -37,6 +21,5 @@ const create = async ({ connectDatas, cultoDatas, numeroPulseira, observacoes })
 }
 
 export {
-    getAll,
-    create
+    associate
 }
