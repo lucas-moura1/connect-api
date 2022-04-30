917157cce4c54a17909465b3771ff094efe14d19
Lucas Moura <lucas3ms.ls@gmail.com>
2022-04-30 10:49:39 -0300

    feat: set log false in sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,8 @@
 import Sequelize from 'sequelize'
 import databaseUrl from '../config/database.js'
 
-const sequelize = new Sequelize(databaseUrl.url)
+const sequelize = new Sequelize(databaseUrl.url, {
+    logging: false
+})
 
 export default sequelize
